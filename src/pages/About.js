f2e82c908717ad25bf04ec9bbdf55b4eb6359c1a
TJ Wu <tjwu24@gmail.com>
2020-07-21 19:57:58 -0700

    revert react-scripts

--- src/pages/About.js
+++ src/pages/About.js
@@ -10,6 +10,8 @@ class About extends Component {
           A picture can is worth a thousand words...
           then how many words is a video worth?
         </div>
+        <iframe src="https://www.youtube.com/embed/21hoOInDVlQ" title="rickroll" frameBorder={0} allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen>
+        </iframe>
       </div>
     );
   }
